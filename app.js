3e31daf7ba8558f882f7899ff20d93e5ea821493
Moldoveanu Alexandra <moldoveanu.alexandra@gmail.com>
2022-04-02 17:28:33 +0300

    added simple ui - buttons and score

--- app.js
+++ app.js
@@ -1,94 +1,99 @@
+let buttons = document.querySelector(".buttons");
+const diplayResult = document.querySelector(".display-result");
+const displayScore = document.querySelector(".display-score");
+let playerScore = 0;
+let computerScore = 0;
+
 const computerPlay = () => {
     let responses = ["rock", "paper", "scissors"];
-    let randomIndex = Math.floor(Math.random()*3);
+    let randomIndex = Math.floor(Math.random() * 3);
     return responses[randomIndex];
 }
-// const winningConditions = {
-//     "rock" : "scissors",
-//     "paper" : "rock",
-//     "scissors": "paper"
-// }
-// const losingConditions = {
-//     "rock" : "paper",
-//     "paper" : "scrissors",
-//     "scissors": "rock"
-// }
+// // const winningConditions = {
+// //     "rock" : "scissors",
+// //     "paper" : "rock",
+// //     "scissors": "paper"
+// // }
+// // const losingConditions = {
+// //     "rock" : "paper",
+// //     "paper" : "scrissors",
+// //     "scissors": "rock"
+// // }
 
-// // playRoundGeneric(winningConditions,losingConditions,"rock",computerPlay())
-// playRoundGeneric = (winningConditions,losingConditions,playerSelection, computerSelection) => {
-//     if (playerSelection === computerSelection) {
-//         return "It's a tie!"
-//     }
-//     for(const key in winningConditions){
-//         if (playerSelection === key && computerSelection === winningConditions[key]){
-//             return `You win! ${playerSelection} beats ${computerSelection}` 
-//         }
-//     }
-//     for(const key in losingConditions){
-//         if (playerSelection === key && computerSelection === winningConditions[key]){
-//             return `You win! ${playerSelection} beats ${computerSelection}` 
-//         }
-//     }
-    
-// }
+// // // playRoundGeneric(winningConditions,losingConditions,"rock",computerPlay())
+// // playRoundGeneric = (winningConditions,losingConditions,playerSelection, computerSelection) => {
+// //     if (playerSelection === computerSelection) {
+// //         return "It's a tie!"
+// //     }
+// //     for(const key in winningConditions){
+// //         if (playerSelection === key && computerSelection === winningConditions[key]){
+// //             return `You win! ${playerSelection} beats ${computerSelection}` 
+// //         }
+// //     }
+// //     for(const key in losingConditions){
+// //         if (playerSelection === key && computerSelection === winningConditions[key]){
+// //             return `You win! ${playerSelection} beats ${computerSelection}` 
+// //         }
+// //     }
+
+// // }
+const playGame = (event) => {
+    const playerSelection = event.target.className;
+    const computerSelection = computerPlay();
+    diplayResult.innerText =  playRound(playerSelection, computerSelection);
+    game();
+}
 
 const playRound = (playerSelection, computerSelection) => {
-    playerSelection = playerSelection.toLowerCase();
+    
+    
     if (playerSelection === computerSelection) {
+        displayScore.innerText = `your score: ${playerScore}  computer score: ${computerScore}`;
         return "It's a tie!"
     }
     if ((playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")) {
-        return `You win! ${playerSelection} beats ${computerSelection}`
+            playerScore++;
+            displayScore.innerText = `your score: ${playerScore}  computer score: ${computerScore}`;
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+       
+        
     }
     if ((playerSelection === "rock" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "scissors") ||
         (playerSelection === "scissors" && computerSelection === "rock")) {
-        return `You lose! ${computerSelection} beats ${playerSelection}`
+            computerScore++;
+            displayScore.innerText = `your score: ${playerScore}  computer score: ${computerScore}`;
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        
     }
 
     throw `Invalid inputs! playerSelection: ${playerSelection} computerSelection: ${computerSelection}`
 
 }
 const game = () => {
-    let numberOfGames = 5;
-    let playerScore = 0;
-    let computerScore = 0;
-    let validOptions = ["rock", "paper", "scissors"]
-    for( let i = 0; i < numberOfGames; i++ ){
-        let playerSelection = prompt("chose an option!").toLowerCase();
-       
-        if(validOptions.indexOf(playerSelection) === -1  ){
-            playerSelection = computerPlay();
-            console.log(`You failed to chose a good option, so ${playerSelection} will be chosen for you`);
-        }
-        const computerSelection = computerPlay();
-        let gameResult = playRound(playerSelection, computerSelection);
-        console.log(playerSelection, computerSelection);
-        if(gameResult.includes("win") ){
-            playerScore ++ ;
-        } 
-        if(gameResult.includes("lose") ){
-            computerScore ++ ;
-        }
+    if (playerScore === 5) {
+        displayScore.innerText = "You are the winner!";
 
+        playerScore = 0;
+        computerScore = 0;
     }
-    if(playerScore > computerScore ) {
-        return "Congrats! You win!";
-    } else if (playerScore < computerScore ){
-        return "You lose!";
+
+    if (computerScore === 5) {
+        displayScore.innerText = "You lose, the computer wins!";
+
+        playerScore = 0;
+        computerScore = 0;
     }
-    else {
-        return "It's a tie!"
-    }
-    
-    
-    
+
 }
-  
-  
-  console.log(game());
- 
 
- 
+for(let i = 0; i < buttons.children.length; i++){
+    
+    let button = buttons.children[i];
+    console.log(button);
+    button.addEventListener("click", playGame);
+}
+
+
